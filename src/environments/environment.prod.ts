9376a5f3426a1974e0026a8e674d95045ff188c8
henry <henrymyil@gmail.com>
2021-03-25 11:31:07 +0200

    Eka versio

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDURPc3OFvR6uM5ojTQpjcG0GeAGx0DuGk",
+    authDomain: "my-dream-app-d934f.firebaseapp.com",
+    projectId: "my-dream-app-d934f",
+    storageBucket: "my-dream-app-d934f.appspot.com",
+    messagingSenderId: "23837524551",
+    appId: "1:23837524551:web:f084d3e7d44b47e7b92762"
+  }
+};
